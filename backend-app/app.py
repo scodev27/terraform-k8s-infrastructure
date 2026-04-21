65a58f422a05d117caba5c3a28faa57df529c72c
gsx <gsx@debian-gsx2.gsx.virtualbox.org>
2026-04-21 18:07:58 +0200

    Simple application container fet

---
+++ backend-app/app.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"Hello from container")
+
+server = HTTPServer(('', 8080), MyHandler)
+server.serve_forever()
